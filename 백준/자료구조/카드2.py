310673a8ee8cf6ad24d8ebe5b82db461df8b53bf
ex1 <skowko6750@naver.com>
2022-07-25 10:48:29 +0900

    day 1

--- 백준/자료구조/카드2.py
+++ 백준/자료구조/카드2.py
@@ -1,24 +1,3 @@
-# n = int(input())
-# arr=[]
-# flag=True
-# for i in range(n):
-#     arr.append(i+1)
-
-
-# while(True):
-#     print(arr)
-#     if len(arr)==1:
-#         break
-#     if len(arr) % 2 == 0:
-#         tmp = True
-#     else: tmp = False
-#     if flag == True: arr=arr[1::2]
-#     else: 
-#         arr=arr[0::2]
-#         arr.append(arr.pop(0))
-#     flag = tmp
-# print(arr[0])
-
 n = int(input())
 arr=[]
 flag=True
