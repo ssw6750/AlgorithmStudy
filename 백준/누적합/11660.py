97afe65b0d0ed0333abddee577ac3c249fc8a264
ex1 <skowko6750@naver.com>
2022-08-13 12:48:12 +0900

    .

---
+++ 백준/누적합/11660.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+arr = [[0 for i in range(n + 1)] for i in range(n + 1)]
+
+mx = [list(map(int, input().split())) for i in range(n)]
+for i in range(n):
+    for j in range(n):
+        arr[i + 1][j + 1] = (arr[i][j + 1] + arr[i + 1][j] - arr[i][j]) + mx[i][j]
+
+for i in range(m):
+    x1, y1, x2, y2 = map(int, input().split())
+    print(arr[x2][y2] - (arr[x1 - 1][y2] + arr[x2][y1 - 1] - arr[x1 - 1][y1 - 1]))
