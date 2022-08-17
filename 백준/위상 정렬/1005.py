1ca91197cdec2d504a57a8db63eb55e88074a6b0
ex1 <skowko6750@naver.com>
2022-08-17 12:08:42 +0900

    .

--- 백준/위상 정렬/1005.py
+++ 백준/위상 정렬/1005.py
@@ -3,32 +3,30 @@ import collections
 
 input = sys.stdin.readline
 
-N = int(input())
-inDegree = [0 for _ in range(N + 1)]
-dp = [0 for _ in range(N + 1)]
-graph = collections.defaultdict(list)
-ar = [0 for _ in range(N + 1)]
+for T in range(int(input())):
+    N, M = map(int, input().split())
+    inDegree = [0 for _ in range(N + 1)]
+    dp = [0 for _ in range(N + 1)]
+    graph = collections.defaultdict(list)
+    ar = [0] + list(map(int, input().split()))
 
-for i in range(1,N+1):
-    tmp = list(map(int, input().split()))
-    ar[i] += tmp[0]
-    for j in range(tmp[1]):
-        graph[tmp[2+j]].append(i)
-        inDegree[i] += 1
+    for _ in range(M):
+        A, B = map(int, input().split())
+        graph[A].append(B)
+        inDegree[B] += 1
 
-
-queue = collections.deque()
-for i in range(1, N + 1):
-    if inDegree[i] == 0:
-        queue.append(i)
-        dp[i] = ar[i]
-
-while queue:
-    node = queue.popleft()
-    for i in graph[node]:
-        inDegree[i] -= 1
-        dp[i] = max(dp[node] + ar[i], dp[i])
+    queue = collections.deque()
+    for i in range(1, N + 1):
         if inDegree[i] == 0:
             queue.append(i)
+            dp[i] = ar[i]
 
-print(max(dp))
+    while queue:
+        node = queue.popleft()
+        for i in graph[node]:
+            inDegree[i] -= 1
+            dp[i] = max(dp[node] + ar[i], dp[i])
+            if inDegree[i] == 0:
+                queue.append(i)
+
+    print(dp[int(input())])
